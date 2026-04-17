b58c802aacda2fd0e954ae04ba2e030532eb7538
krasenamarkova <kraschoo@abv.bg>
2026-04-17 12:09:00 +0300

    feat: add encapsulation lab 02 solutions

---
+++ src/encapsulation_lab02/firstandreserveteam04/Team.java
@@ -0,0 +1,38 @@
+package encapsulation_lab02.firstandreserveteam04;
+
+import encapsulation_lab02.sortbynameandage01.Person;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(String name) {
+        this.name = name;
+        this.firstTeam = new ArrayList<>();
+        this.reserveTeam = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addPlayer(Person p) {
+        if (p.getAge() < 40) {
+            firstTeam.add(p);
+            return;
+        }
+        reserveTeam.add(p);
+    }
+
+    public List<Person> getFirstTeam() {
+        return Collections.unmodifiableList(firstTeam);
+    }
+    public List<Person> getReserveTeam() {
+        return Collections.unmodifiableList(reserveTeam);
+    }
+}
